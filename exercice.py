ae57af2befb23e22d7dc844159d53de2fae61889
Benoit323 <70984311+Benoit323@users.noreply.github.com>
2020-10-05 19:22:19 -0400

    1

--- exercice.py
+++ exercice.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import matplotlib
 from matplotlib.colors import cnames
 
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-
-    return {}
+    mydict = dict()
+    for index, elem in enumerate(some_list):
+        mydict[elem] = index
+    return mydict
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    mylist = []
+    for i in colors:
+        hex_colors = matplotlib.colors.cnames[i]
+        mylist.append(tuple([i, hex_colors]))
+    return mylist
 
 
 def create_list() -> list:
+    my_list  = []
     # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
-
-    return []
+    my_list = [i for i in range(1, 10000) if i < 15 or i > 350]
+    """for i in range(1,10000):
+        if i < 15 or i > 350:
+            my_list.append(i)
+        else:
+            continue"""
+    return my_list
 
 
 def compute_mse(model_dict: dict) -> dict:
